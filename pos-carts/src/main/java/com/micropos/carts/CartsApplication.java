675bb33601de8cf4c5fb4be8681797f4ee0c444b
wanghr <239955612@qq.com>
2022-04-23 19:46:38 +0800

    finished coding, not fully debugged

--- pos-carts/src/main/java/com/micropos/carts/CartsApplication.java
+++ pos-carts/src/main/java/com/micropos/carts/CartsApplication.java
@@ -1,4 +1,4 @@
-package com.micropos.products;
+package com.micropos.carts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProductsApplication {
+public class CartsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProductsApplication.class, args);
+        SpringApplication.run(CartsApplication.class, args);
     }
 }
